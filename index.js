efdf85fc567423954bbcaf1714d8303c1d9a3fed
Christopher MBogo <chrismbogo97@gmail.com>
2022-08-05 08:51:34 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (string){
+    return string.toUpperCase()
+}
+console .log(shout ("Hello!"));
+
+function whisper (string){
+    return string.toLowerCase()
+}
+console.log(whisper("Hello!"));
+
+function logShout(hello) {
+    console.log (hello.toUpperCase(hello));
+}
+logShout("hello");
+
+function logWhisper(HELLO) {
+    console.log (HELLO.toLowerCase(HELLO))
+}logShout("hello")
+function sayHiToHeadphonedRoommate (string) {
+  if (string===string.toLowerCase()) {return "I can\'t hear you!"
+} 
+if (string===string.toUpperCase()) {return "YES INDEED!"
+} 
+if (string==="Let\'s have dinner together!") {return "I would love to!"
+}  } 
+console.log(sayHiToHeadphonedRoommate("Let\'s have dinner together!"));
